9e716e7136ba0fccfcd4ca416972424fda033bcb
ilamaksimov <Maksimov.I@citilink.ru>
2023-06-03 20:40:33 +0300

    isSymmetric

--- notion-2-week/leetcode/trees/isSymmetric.go
+++ notion-2-week/leetcode/trees/isSymmetric.go
@@ -1,10 +1,19 @@
 package trees
 
-// TODO доделать !
+// time O(nLogn)
+// memory O(1)
 func isSymmetric(root *TreeNode) bool {
-	if root.Left != nil && root.Right != nil {
-		return isSymmetric(root.Left) == isSymmetric(root.Right)
+	return dsf(root.Left, root.Right)
+}
+
+func dsf(left *TreeNode, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
 	}
 
-	return false
+	if left == nil || right == nil {
+		return false
+	}
+
+	return left.Val == right.Val && dsf(left.Left, right.Right) && dsf(left.Right, right.Left)
 }
