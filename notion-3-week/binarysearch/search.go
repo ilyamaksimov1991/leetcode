cd4367ed33d9505e727b07c68154ff88d63e91e5
ilamaksimov <Maksimov.I@citilink.ru>
2023-06-12 09:36:39 +0300

    search2

---
+++ notion-3-week/binarysearch/search.go
@@ -0,0 +1,32 @@
+package binarysearch
+
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+// time O(logn)
+// memory O(1)
+func search2(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[left] > nums[mid] {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		} else {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		}
+	}
+
+	return -1
+}
